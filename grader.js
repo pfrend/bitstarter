c45282be4a0980ee8d004f56190402f2ebbb987e
pfrend <paul.cf@hotmail.com>
2013-07-13 10:17:21 +0000

    grader updated to take URL

--- grader.js
+++ grader.js
@@ -4,7 +4,7 @@ Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
 
-References:
+nReferences:
 
  + cheerio
    - https://github.com/MatthewMueller/cheerio
@@ -22,10 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+//var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "http://guarded-eyrie-2495.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +36,22 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var getDataFromURL = function(url,checks) {
+    var res = "";
+    rest.get(url).on('complete',function(result){
+	if (result instanceof Error) {
+	    console.log('Error: ' + result.message);
+	    process.exit(1);
+	    //this.retry(5000); // try again after 5 sec
+	} else {
+	    
+	    var checkJson = checkUrlData(result,checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);	
+	    console.log(outJson);
+	}
+    });
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -55,20 +73,51 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var cheerioURL = function(urlData){
+return cheerio.load(urlData);
+};
+
+var checkUrlData = function(urldata, checksfile) {
+    $ = cheerioURL(urldata);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+
+    return out;
+
+   };
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+	.option('-u, --url <url>', 'Path to url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.file) {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+	process.exit(0);
+    }
+
+    if(program.url) {
+	//console.log('hit');
+
+	getDataFromURL(program.url,program.checks);
+
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
